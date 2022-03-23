1c4fdf554a529546d5a4a3b1b4ab49166feb9e45
maryam matiri <64448234+Lalique-art@users.noreply.github.com>
2022-03-23 20:09:24 +0300

    done

--- day7-MovieApp/day7-MovieApp/ContentView.swift
+++ day7-MovieApp/day7-MovieApp/ContentView.swift
@@ -8,14 +8,37 @@
 import SwiftUI
 
 struct ContentView: View {
+    var films = ["howl","sophie","kid","howls moving castle"]
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView{
+        List(films,id:\.self){film in
+            NavigationLink(destination: detailsView()){
+            movierow(film: film)
+            }
+        }.navigationTitle("ghibli")
+        }
     }
 }
+            
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct movierow: View {
+    var film: String
+    var body: some View {
+        HStack{
+            Image(film)
+                .resizable()
+                .scaledToFill()
+                .frame(width: 100, height: 100)
+                .clipShape(Circle())
+            Text(film)
+                .font(.headline)
+                .fontWeight(.bold)
+        }
     }
 }
